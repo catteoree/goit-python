e25d98b458d6dff2f6c1aecb5b2dee0d4ef65cd1
catteoree <catteoree@gmail.com>
2021-10-27 12:53:52 +0300

    Home Work 10: lesson tasks

---
+++ lesson10/m10py03figure.py
@@ -0,0 +1,52 @@
+from cmath import pi
+
+
+class Figure:
+    sides = []
+
+    def area(self):
+        return "Some area"
+
+    def get_sides(self):
+        return self.sides
+
+    def set_sides(self, list_of_sides):
+        self.sides = list_of_sides
+
+
+class Rectangle(Figure):
+    sides = [1, 2]
+
+    def area(self):
+        return self.sides[0] * self.sides[1]
+
+
+class Triangle(Figure):
+    pass
+
+
+class Circle(Figure):
+    radius = 5
+
+    def area(self):
+        return pi * (self.radius ** 2)
+
+
+class SomeFigure(Figure):
+    sides = [3, 5, 3, 2, 7]
+
+
+c = Circle()
+print(c.sides)
+print(c.radius)
+print(c.area())
+
+r = Rectangle()
+print(r.sides)
+print(r.area())
+
+sf = SomeFigure()
+print(sf.sides)
+print(sf.area())
+
+
---
+++ lesson10/m10py03import_string.py
@@ -0,0 +1,13 @@
+import string
+
+
+print(string.digits)
+print(string.ascii_letters)
+print(string.hexdigits)
+print(string.printable)
+print(string.whitespace)
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string.octdigits)
+print(string.punctuation)
+print(" " in string.printable)
