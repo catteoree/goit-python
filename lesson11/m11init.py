18dc721de501e183c44fe07016a665e75f705961
catteoree <catteoree@gmail.com>
2021-10-28 12:01:38 +0300

    Home Work 10: init and str

---
+++ lesson11/m11init.py
@@ -0,0 +1,13 @@
+class Human:
+    def __init__(self, name, age=None):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return  f"Human, name: {self.name}, age: {self.age}"
+
+
+bob = Human('Bob', 20)
+print(bob)
+
+
