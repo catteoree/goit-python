26748645e9a94edede590f8194a46ef18a2e66c6
catteoree <catteoree@gmail.com>
2021-11-01 11:09:35 +0200

    Home Work 08

--- lesson08/m08_py04.py
+++ lesson08/m08_py04.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 import pytz
 import money
 
-
 # print(executable)
 time_utc = datetime.now(tz=pytz.timezone('UTC'))    # Coordinated Universal Time
 str_time_utc = time_utc.strftime('%Y/%m/%d %H:%M:%S %Z %z')
@@ -12,11 +11,8 @@ delta = timedelta(hours=2)
 str_time_delta = time_utc + delta
 str_time_delta = str_time_delta.strftime('%Y/%m/%d %H:%M:%S %Z %z')
 
-
 print(time_utc)
 print(str_time_utc)
 print(str_time_delta)
 
-
 print(help(money))
-
