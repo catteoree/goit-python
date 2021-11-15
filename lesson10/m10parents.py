4240f575a95943a96a38a8249d7b3a769809c840
catteoree <catteoree@gmail.com>
2021-11-15 15:40:13 +0300

    test super()

--- lesson10/m10parents.py
+++ lesson10/m10parents.py
@@ -11,10 +11,10 @@ class Parent2:
         print("This is parent2")
 
 
-class Child(Parent1):
+class Child(Parent1, Parent2):
     def __init__(self, name, age):
-        super(Parent1, self).__init__(name)
-        super(Parent2, self).__init__()
+        super(Child, self).__init__(name)
+        # super(Parent2, self).__init__()
         self.age = age
         # self.name = name
 
