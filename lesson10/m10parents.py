9cdc15e5758a51cf007b9e9cc4ac4183fcb91e80
catteoree <catteoree@gmail.com>
2021-11-15 14:54:42 +0300

    Testing commit: incorrect file

---
+++ lesson10/m10parents.py
@@ -0,0 +1,25 @@
+class Parent1:
+    def __init__(self, name):
+        self.name = name
+
+    def say_name(self):
+        print(f"Hi! My name is {self.name}.")
+
+
+class Parent2:
+    def __init__(self):
+        print("This is parent2")
+
+
+class Child(Parent1):
+    def __init__(self, name, age):
+        super(Parent1, self).__init__(name)
+        super(Parent2, self).__init__()
+        self.age = age
+        # self.name = name
+
+
+child = Child("Job", 25)
+child.say_name()
+# parent2 = Parent2()
+# parent2.__init__()
